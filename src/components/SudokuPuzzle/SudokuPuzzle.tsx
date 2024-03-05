9034081041344f050d54e536c146a97fccf68bf9
Aien Pahlevan <aeenpahlevanhim@gmail.com>
2024-03-05 23:06:43 +0330

    add primary structure for sudoku puzzle.

--- src/components/SudokuPuzzle/SudokuPuzzle.tsx
+++ src/components/SudokuPuzzle/SudokuPuzzle.tsx
@@ -1,10 +1,109 @@
+import { Grid } from "@mui/material";
+import "./style.css";
+
 function SudokuPuzzle() {
   return (
-    <div>
-      <div>
-        <input type="text" />
-      </div>
-    </div>
+    <Grid container width="320px" boxSizing="border-box">
+      <Grid item xs={4} border="1px solid black">
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+      </Grid>
+      <Grid item xs={4} border="1px solid black">
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+      </Grid>
+      <Grid item xs={4} border="1px solid black">
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+      </Grid>
+      <Grid item xs={4} border="1px solid black">
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+      </Grid>
+      <Grid item xs={4} border="1px solid black">
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+      </Grid>
+      <Grid item xs={4} border="1px solid black">
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+      </Grid>
+      <Grid item xs={4} border="1px solid black">
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+      </Grid>
+      <Grid item xs={4} border="1px solid black">
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+      </Grid>
+      <Grid item xs={4} border="1px solid black">
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+        <input type="number" />
+      </Grid>
+    </Grid>
   );
 }
 
