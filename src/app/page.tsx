1d2318b886f1df9462b9d36d0cc0bcc66c0b7dc5
Aien Pahlevan <aeenpahlevanhim@gmail.com>
2024-03-18 20:24:15 +0330

    fix names.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
 
   return (
     <Stack alignItems="center" justifyContent="center" height="100vh" gap={4}>
-      <Typography variant="h2">Welcome to Sudoco</Typography>
-      <Button onClick={() => router.push("/sudoko-game")}>
+      <Typography variant="h2">Welcome to Sudoku</Typography>
+      <Button onClick={() => router.push("/sudoku-game")}>
         Start the Game
       </Button>
     </Stack>
