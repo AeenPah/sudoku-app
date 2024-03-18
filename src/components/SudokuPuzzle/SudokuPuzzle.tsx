21ac2122ab7eeaec04bbbcd8339b1deefeee61a2
Aien Pahlevan <aeenpahlevanhim@gmail.com>
2024-03-18 13:28:02 +0330

    create watch data in inputs and make them red by wrong numbers.

--- src/components/SudokuPuzzle/SudokuPuzzle.tsx
+++ src/components/SudokuPuzzle/SudokuPuzzle.tsx
@@ -9,7 +9,7 @@ import "./style.css";
 /*                                    Type                                    */
 /* -------------------------------------------------------------------------- */
 
-type TSquareList = {
+type TSquareGroup = {
   one: boolean;
   two: boolean;
   three: boolean;
@@ -21,11 +21,16 @@ type TSquareList = {
   nine: boolean;
 };
 
+type TInputValue = {
+  value: string;
+  isWrongAnswer?: boolean;
+};
+
 /* -------------------------------------------------------------------------- */
 /*                                Initial Data                                */
 /* -------------------------------------------------------------------------- */
 
-const initialSquareList: TSquareList = {
+const initialSquareGroup: TSquareGroup = {
   one: false,
   two: false,
   three: false,
@@ -37,71 +42,198 @@ const initialSquareList: TSquareList = {
   nine: false,
 };
 
+const initialInputValue: TInputValue = {
+  value: "",
+};
+
 function SudokuPuzzle() {
   /* -------------------------------------------------------------------------- */
   /*                                    State                                   */
   /* -------------------------------------------------------------------------- */
 
-  const [squareList1, setSquareList1] =
-    useState<TSquareList>(initialSquareList);
+  const [squareGroup1, setSquareGroup1] =
+    useState<TSquareGroup>(initialSquareGroup);
+
+  const [inputValue1, setInputValue1] =
+    useState<TInputValue>(initialInputValue);
+  const [inputValue2, setInputValue2] =
+    useState<TInputValue>(initialInputValue);
+  const [inputValue3, setInputValue3] =
+    useState<TInputValue>(initialInputValue);
+  const [inputValue4, setInputValue4] =
+    useState<TInputValue>(initialInputValue);
+  const [inputValue5, setInputValue5] =
+    useState<TInputValue>(initialInputValue);
+  const [inputValue6, setInputValue6] =
+    useState<TInputValue>(initialInputValue);
+  const [inputValue7, setInputValue7] =
+    useState<TInputValue>(initialInputValue);
+  const [inputValue8, setInputValue8] =
+    useState<TInputValue>(initialInputValue);
+  const [inputValue9, setInputValue9] =
+    useState<TInputValue>(initialInputValue);
 
   /* -------------------------------------------------------------------------- */
   /*                                  Function                                  */
   /* -------------------------------------------------------------------------- */
 
-  function handleOnClick(value: string, inputId: number) {
-    console.log("inputId", inputId);
+  function handleAddNumber(
+    value: string,
+    inputId: number,
+    inputGroupId: number
+  ) {
+    console.log("inputGroupId", inputGroupId);
+
+    let setInputValueFunction;
+
+    // Set input state to function.
+    switch (inputId) {
+      case 1:
+        setInputValueFunction = setInputValue1;
+        break;
+      case 2:
+        setInputValueFunction = setInputValue2;
+        break;
+      case 3:
+        setInputValueFunction = setInputValue3;
+        break;
+      case 4:
+        setInputValueFunction = setInputValue4;
+        break;
+      case 5:
+        setInputValueFunction = setInputValue5;
+        break;
+      case 6:
+        setInputValueFunction = setInputValue6;
+        break;
+      case 7:
+        setInputValueFunction = setInputValue7;
+        break;
+      case 8:
+        setInputValueFunction = setInputValue8;
+        break;
+      case 9:
+        setInputValueFunction = setInputValue9;
+        break;
+      default:
+        // TODO: Change this
+        setInputValueFunction = () => {};
+        break;
+    }
+
+    setInputValueFunction({
+      value: value,
+      // isWrongAnswer: true,
+    });
 
     switch (value) {
       case "1":
-        setSquareList1({ ...squareList1, one: true });
+        if (!squareGroup1.one) setSquareGroup1({ ...squareGroup1, one: true });
+        else setInputValueFunction({ ...inputValue1, isWrongAnswer: true });
         break;
       case "2":
-        setSquareList1({ ...squareList1, two: true });
+        if (!squareGroup1.two) setSquareGroup1({ ...squareGroup1, two: true });
+        else setInputValueFunction({ ...inputValue1, isWrongAnswer: true });
         break;
       case "3":
-        setSquareList1({ ...squareList1, three: true });
+        if (!squareGroup1.three)
+          setSquareGroup1({ ...squareGroup1, three: true });
+        else setInputValueFunction({ ...inputValue1, isWrongAnswer: true });
         break;
       case "4":
-        setSquareList1({ ...squareList1, four: true });
+        if (!squareGroup1.four)
+          setSquareGroup1({ ...squareGroup1, four: true });
+        else setInputValueFunction({ ...inputValue1, isWrongAnswer: true });
         break;
       case "5":
-        setSquareList1({ ...squareList1, five: true });
+        if (!squareGroup1.five)
+          setSquareGroup1({ ...squareGroup1, five: true });
+        else setInputValueFunction({ ...inputValue1, isWrongAnswer: true });
         break;
       case "6":
-        setSquareList1({ ...squareList1, six: true });
+        if (!squareGroup1.six) setSquareGroup1({ ...squareGroup1, six: true });
+        else setInputValueFunction({ ...inputValue1, isWrongAnswer: true });
         break;
       case "7":
-        setSquareList1({ ...squareList1, seven: true });
+        if (!squareGroup1.seven)
+          setSquareGroup1({ ...squareGroup1, seven: true });
+        else setInputValueFunction({ ...inputValue1, isWrongAnswer: true });
         break;
       case "8":
-        setSquareList1({ ...squareList1, eight: true });
+        if (!squareGroup1.eight)
+          setSquareGroup1({ ...squareGroup1, eight: true });
+        else setInputValueFunction({ ...inputValue1, isWrongAnswer: true });
         break;
       case "9":
-        setSquareList1({ ...squareList1, nine: true });
+        if (!squareGroup1.nine)
+          setSquareGroup1({ ...squareGroup1, nine: true });
+        else setInputValueFunction({ ...inputValue1, isWrongAnswer: true });
         break;
     }
   }
 
+  //TODO: Delete this later.
   useEffect(() => {
-    console.log("squareList1", squareList1);
-  }, [squareList1]);
+    console.log("squareList1", squareGroup1);
+  }, [squareGroup1]);
 
   return (
     <Grid container width="320px" boxSizing="border-box">
       <Grid item xs={4} border="1px solid black">
         <input
+          className={inputValue1.isWrongAnswer ? "wrong-answer" : ""}
+          disabled={inputValue1.value !== ""}
+          onChange={(e) => handleAddNumber(e.target.value, 1, 1)}
           type="number"
-          onChange={(e) => handleOnClick(e.target.value, 1)}
         />
-        <input type="number" />
-        <input type="number" />
-        <input type="number" />
-        <input type="number" />
-        <input type="number" />
-        <input type="number" />
-        <input type="number" />
-        <input type="number" />
+        <input
+          disabled={inputValue2.value !== ""}
+          className={inputValue2.isWrongAnswer ? "wrong-answer" : ""}
+          onChange={(e) => handleAddNumber(e.target.value, 2, 1)}
+          type="number"
+        />
+        <input
+          disabled={inputValue3.value !== ""}
+          className={inputValue3.isWrongAnswer ? "wrong-answer" : ""}
+          onChange={(e) => handleAddNumber(e.target.value, 3, 1)}
+          type="number"
+        />
+        <input
+          disabled={inputValue4.value !== ""}
+          className={inputValue4.isWrongAnswer ? "wrong-answer" : ""}
+          onChange={(e) => handleAddNumber(e.target.value, 4, 1)}
+          type="number"
+        />
+        <input
+          disabled={inputValue5.value !== ""}
+          className={inputValue5.isWrongAnswer ? "wrong-answer" : ""}
+          onChange={(e) => handleAddNumber(e.target.value, 5, 1)}
+          type="number"
+        />
+        <input
+          disabled={inputValue6.value !== ""}
+          className={inputValue6.isWrongAnswer ? "wrong-answer" : ""}
+          onChange={(e) => handleAddNumber(e.target.value, 6, 1)}
+          type="number"
+        />
+        <input
+          disabled={inputValue7.value !== ""}
+          className={inputValue7.isWrongAnswer ? "wrong-answer" : ""}
+          onChange={(e) => handleAddNumber(e.target.value, 7, 1)}
+          type="number"
+        />
+        <input
+          disabled={inputValue8.value !== ""}
+          className={inputValue8.isWrongAnswer ? "wrong-answer" : ""}
+          onChange={(e) => handleAddNumber(e.target.value, 8, 1)}
+          type="number"
+        />
+        <input
+          disabled={inputValue9.value !== ""}
+          className={inputValue9.isWrongAnswer ? "wrong-answer" : ""}
+          onChange={(e) => handleAddNumber(e.target.value, 9, 1)}
+          type="number"
+        />
       </Grid>
       <Grid item xs={4} border="1px solid black">
         <input type="number" />
