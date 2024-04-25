7cf7b25a9ae90113ad07dfc57827f0850a9b1d03
Aien Pahlevan <aeenpahlevanhim@gmail.com>
2024-04-25 23:02:38 +0330

    add mui theme provider and them file.

---
+++ src/theme.tsx
@@ -0,0 +1,12 @@
+import {
+  ThemeOptions as MuiThemeOptions,
+  PaletteOptions,
+} from "@mui/material/styles";
+
+const paletteColors: PaletteOptions = {};
+
+const ThemeOptions: MuiThemeOptions = {
+  palette: paletteColors,
+};
+
+export default ThemeOptions;
